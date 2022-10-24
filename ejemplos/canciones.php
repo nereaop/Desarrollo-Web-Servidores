1d41544c5eeca6ee544b0c74d13f39528e7bc99f
nereaop <nereagamermh@gmail.com>
2022-10-24 15:35:34 +0200

    ejemplos

---
+++ ejemplos/canciones.php
@@ -0,0 +1,12 @@
+<?php
+$vocales = array["a","e","i","o","u"];
+
+$cadena = "El sapo no se lava el pie";
+
+$contador = strlen($cadena);
+
+function escribirFrase($frase, $vocal){
+    for ($i=0; $i < $contador ; $i++) { 
+        
+    }
+}
