1aec5744cc38f99093808c67fa660c5037b3a29d
nereaop <nereagamermh@gmail.com>
2022-10-25 20:03:10 +0200

    ejercicio canciones y creacion de los documentos de la practica

--- ejemplos/canciones.php
+++ ejemplos/canciones.php
@@ -1,12 +1,43 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Desarrollo de aplicaciones web en entorno servidor</title> 
+</head>
+<body>
+<div> Curso de PHP - Práctica: canción infantil.</div>
 <?php
-$vocales = array["a","e","i","o","u"];
+    ini_set('display_errors', 'On');
+    ini_set('html_errors', 0);
+    $palabras = ["el", "sapo", "no", "se","lava","el","pie."];
+    $vocales = ["a", "e", "i", "o","u"];
+?>
 
-$cadena = "El sapo no se lava el pie";
 
-$contador = strlen($cadena);
-
-function escribirFrase($frase, $vocal){
-    for ($i=0; $i < $contador ; $i++) { 
-        
+<?php
+    function escribir($word,$vocal)
+    {
+        $length = strlen($word);
+        for ($i=0; $i<$length;$i++)
+            if (in_array($word[$i], array("a", "e", "i", "o","u")))
+                echo $vocal;
+            else
+                echo $word[$i];
     }
-}
+?>
+
+<?php
+    echo "<div>";
+    foreach ($palabras as $p)
+        echo $p." ";
+    echo "</div>";
+
+    foreach ($vocales as $v)
+    {   echo "<div>";
+        foreach ($palabras as $p)
+            echo escribir($p,$v)." ";
+        echo "</div>";
+    }
+?>
+</body>
+</html>
