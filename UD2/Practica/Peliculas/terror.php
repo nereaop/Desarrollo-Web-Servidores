cb4f384c48e2b283c69ae886b40e43ede548920b
nereaop <nereagamermh@gmail.com>
2022-11-15 20:31:58 +0100

    Primera pelicula sale bien

---
+++ UD2/Practica/Peliculas/terror.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>Inicio</title>
+    <link href="https://fonts.googleapis.com/css2?family=Denk+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<div class="contenedor">
+        <div class="navBar">
+        </div>
+        <div class="segunda_caja">
+           <div class="primera_columna">
+            <div class="contenedorImagen">
+            <h1>ANNABELLE</h1>
+            <img src="imagenes/annabelle.jpg" alt="ANNABELLE" class="imagen">
+           </div>
+</div>
+           <div class="segunda_columna">
+            <p>holaa</p>
+           </div>
+        </div>
+    <div>
+</body>
+</html>
+
